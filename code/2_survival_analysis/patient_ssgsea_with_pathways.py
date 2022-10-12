ba4d4fa150b64778db73bd330446172dc3b611d0
SBIlab <91528996+SBIlab@users.noreply.github.com>
2022-10-12 10:10:43 +0900

    Update patient_ssgsea_with_pathways.py

--- code/2_survival_analysis/patient_ssgsea_with_pathways.py
+++ code/2_survival_analysis/patient_ssgsea_with_pathways.py
@@ -10,13 +10,13 @@ for gs in GENESET:
     TYPE = os.listdir(PATH)
     TYPE.remove("Reactome")
     print(TYPE)
-    try: os.mkdir("../data/" + gs)
+    try: os.mkdir("../../data/" + gs)
     except: pass
     for ty in TYPE:
         print(ty[5:])
         em = pd.read_table(PATH+ty+"/GeTMM_log2_rna_seq.txt", index_col="genes")
-        ssres = gp.ssgsea(em, "../data/Reactome.v7.0.ensembl.gmt",
-                             outdir="../result/pagerank/gsea/", sample_norm_method='rank',
+        ssres = gp.ssgsea(em, "../../data/Reactome.v7.0.ensembl.gmt",
+                             outdir="../../result/pagerank/gsea/", sample_norm_method='rank',
                              permutation_num=0, processes=5,no_plot=True, min_size=5, seed=10)
         re = ssres.res2d.sort_index()
         print(re)
